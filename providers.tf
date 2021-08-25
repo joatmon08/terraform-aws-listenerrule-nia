994acd288ba7fdfeaf9bfd0607705f3721b28a8c
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2021-08-25 16:26:22 -0400

    Upgrade to Terraform 1.0

--- providers.tf
+++ providers.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">=0.14"
+  required_version = ">=1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">=3.23"
+      version = ">=3.55"
     }
   }
 }
